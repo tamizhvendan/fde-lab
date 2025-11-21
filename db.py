b6e1820911d129a714bc003d4ff93e0f5e262220
Tamizhvendan S <tamizh88@gmail.com>
2025-11-21 05:44:13 +0530

    refactor

---
+++ db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from config import settings
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(str(settings.DATABASE_URL))
+
+def get_db_session():
+  return sessionmaker(bind=engine)()
+
